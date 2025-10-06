f8ead5eec718460c992cafac95fd74203570e5c1
ceorl <dev.rocher.florian@gmail.com>
2025-10-06 15:05:50 +0200

    changement RedirectViews pour def redirection. Redirection si connecté Redirection après logout

--- litrevu/authentication/urls.py
+++ litrevu/authentication/urls.py
@@ -1,18 +1,19 @@
 from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path
-from django.views.generic import RedirectView
 
-from .views import SignupView
+from .views import SignupView, redirection
 
 app_name = 'authentication'
 
 urlpatterns = [
-    path('', RedirectView.as_view(pattern_name='authentication:login', permanent=False)),
+    path('', redirection, name='redirect'),
+
     path('login/',
          LoginView.as_view(
-             template_name='authentication/login.html',
+            template_name='authentication/login.html',
+            redirect_authenticated_user=True,
          ),
          name='login'),
-    path('logout/', LogoutView.as_view(),name='logout'),
+    path('logout/', LogoutView.as_view(next_page='authentication:login'),name='logout'),
     path('signup/', SignupView.as_view(),name='signup'),
 ]
