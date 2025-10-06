0ca3bf4497905f3d607ab214ce19a2e767981362
ceorl <dev.rocher.florian@gmail.com>
2025-10-06 13:09:12 +0200

    add models ==> Ticket, Review, UserFollow, User

--- litrevu/reviews/models.py
+++ litrevu/reviews/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+class Ticket(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=2048)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField( null=True, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=128)
+    body = models.TextField(max_length=8192, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.headline
+
+
+class UserFollow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    following_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+
+    class Meta:
+        unique_together = ('user', 'following_user')
