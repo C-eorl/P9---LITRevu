e38742270e12af0647102e1ea3433c1e035b6423
ceorl <dev.rocher.florian@gmail.com>
2025-10-07 09:52:05 +0200

    modification LoginView pour CustomLoginView

--- litrevu/authentication/urls.py
+++ litrevu/authentication/urls.py
@@ -1,7 +1,7 @@
-from django.contrib.auth.views import LogoutView, LoginView
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from .views import SignupView, redirection
+from .views import SignupView, redirection, CustomLoginView
 
 app_name = 'authentication'
 
@@ -9,11 +9,7 @@ urlpatterns = [
     path('', redirection, name='redirect'),
 
     path('login/',
-         LoginView.as_view(
-            template_name='authentication/login.html',
-            redirect_authenticated_user=True,
-         ),
-         name='login'),
+         CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='authentication:login'),name='logout'),
     path('signup/', SignupView.as_view(),name='signup'),
 ]
