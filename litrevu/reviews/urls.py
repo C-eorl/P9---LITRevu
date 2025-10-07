1b6c16f1c222386a89b6db4c0a5e629ca9f3e464
ceorl <dev.rocher.florian@gmail.com>
2025-10-07 16:34:49 +0200

    add chemin follow

--- litrevu/reviews/urls.py
+++ litrevu/reviews/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from .views import FeedView
+from .views import FeedView, FollowView, follow_user, unfollow_user
 
 app_name = 'reviews'
 
 urlpatterns = [
     path("", FeedView.as_view(), name='feed'),
+    path("follow/", FollowView.as_view(), name='follow'),
+    path('follow/<int:user_id>/', follow_user, name='follow_user'),
+    path('unfollow/<int:user_id>/', unfollow_user, name='unfollow_user'),
+    path('follow_user/', follow_user, name='follow_user'),
+
 ]
