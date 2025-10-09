719ddd248a301a5e1d3b1bceb9ab26b011bf409f
ceorl <dev.rocher.florian@gmail.com>
2025-10-09 15:41:07 +0200

    modification model user pour image

--- litrevu/authentication/models.py
+++ litrevu/authentication/models.py
@@ -3,4 +3,4 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     """ Utilisateur personnalisé """
-    profile_picture = models.ImageField(upload_to="", null=True, blank=True)
+    profile_picture = models.ImageField(upload_to="avatars/", null=True, blank=True)
