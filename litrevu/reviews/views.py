c3aad205408a264e21286085043990d56dd0fc01
ceorl <dev.rocher.florian@gmail.com>
2025-10-10 11:45:06 +0200

    implentation des views Ticket => create, modify & delete Review => create, modify, delete
    refact. f() search, follow

--- litrevu/reviews/views.py
+++ litrevu/reviews/views.py
@@ -1,9 +1,12 @@
+from django.contrib import messages
+from django.forms.models import modelform_factory
 from django.http import JsonResponse
 from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.views.decorators.http import require_GET, require_POST
-from django.views.generic import ListView, TemplateView
+from django.views.generic import ListView, TemplateView, CreateView, UpdateView, DeleteView
 
-from .models import UserFollow
+from .models import UserFollow, Ticket, Review
 from authentication.models import User
 
 
@@ -14,8 +17,76 @@ class FeedView(TemplateView):
 class PostView(ListView):
     pass
 
+# ================================================================ #
+#                         Ticket                                   #
+# ================================================================ #
+class TicketCreateView(CreateView):
+    template_name = 'reviews/ticket_form.html'
+    success_url = reverse_lazy('reviews:feed')
+    model = Ticket
+    fields = ['title', 'description', 'image']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        response = super().form_valid(form)
+        messages.success(self.request, 'Ticket créé avec succès!')
+        return response
 
 
+class TicketModifyView(UpdateView):
+    template_name = 'reviews/ticket_form.html'
+
+
+class TicketDeleteView(DeleteView):
+    template_name = 'reviews/ticket_delete.html'
+    pass
+
+
+# ================================================================ #
+#                         Critique                                 #
+# ================================================================ #
+class ReviewView(CreateView):
+    template_name = 'reviews/review_form.html'
+    success_url = reverse_lazy('reviews:feed')
+    model = Review
+    fields = ['headline', 'rating', 'body']
+    TicketForm = modelform_factory(Ticket, fields=['title', 'description', 'image'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.POST:
+            context["ticket_form"] = self.TicketForm(self.request.POST, self.request.FILES, prefix="ticket")
+        else:
+            context["ticket_form"] = self.TicketForm(prefix="ticket")
+        return context
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+
+        ticket_form = self.TicketForm(self.request.POST, self.request.FILES, prefix="ticket")
+
+        if ticket_form.is_valid():
+            ticket = ticket_form.save(commit=False)
+            ticket.user = self.request.user
+            ticket.save()
+            form.instance.ticket = ticket
+        else:
+            return self.form_invalid(form)
+
+        messages.success(self.request, "Ticket et Critique créés avec succès !")
+        return super().form_valid(form)
+
+
+class ReviewModifyView(UpdateView):
+    template_name = 'reviews/review_modify.html'
+    pass
+
+class ReviewDeleteView(DeleteView):
+    template_name = 'reviews/review_delete.html'
+    pass
+# ================================================================ #
+#                         Abonnements                              #
+# ================================================================ #
 class FollowView(TemplateView):
     template_name = 'reviews/follow.html'
 
@@ -28,9 +99,7 @@ class FollowView(TemplateView):
 
 
 def unfollow_user(request, user_id):
-    """
-    Unfollow a user
-    """
+    """ View pour se désabonner d'un utilisateur """
     user_to_unfollow = User.objects.get(pk=user_id)
     UserFollow.objects.filter(user=request.user, following_user=user_to_unfollow).delete()
     return redirect('reviews:follow')
@@ -38,35 +107,25 @@ def unfollow_user(request, user_id):
 
 @require_POST
 def follow_user(request):
-    """
-    follow a user
-    """
+    """ View pour s'abonner d'un utilisateur """
     username = request.POST.get("username")
     if not username:
         return JsonResponse({"success": False, "error": "Nom d'utilisateur manquant."})
 
-    try:
-        user_to_follow = User.objects.get(username=username)
-        if user_to_follow == request.user:
-            return JsonResponse({"success": False, "error": "Vous ne pouvez pas vous abonner à vous-même."})
+    user_to_follow = User.objects.get(username=username)
+    UserFollow.objects.get_or_create(user=request.user, following_user=user_to_follow)
+    return JsonResponse({"success": True})
 
-        UserFollow.objects.get_or_create(user=request.user, following_user=user_to_follow)
-        return JsonResponse({"success": True})
-    except User.DoesNotExist:
-        return JsonResponse({"success": False, "error": "Utilisateur introuvable."})
 
 
 @require_GET
 def search_user(request):
-    """
-    Search user
-    """
+    """ View pour rechercher un utilisateur """
+
+    # filtre la recherche
     query = request.GET.get("q", "")
-
-    # IDs des utilisateurs que je suis déjà
-    followed_ids = UserFollow.objects.filter(user=request.user).values_list('following_user_id', flat=True)
-
-    # Recherche des utilisateurs correspondant à la query, en excluant moi-même et ceux que je suis déjà
+    followed_ids = UserFollow.objects.filter(user=request.user) \
+                                     .values_list('following_user_id', flat=True)
     users = User.objects.filter(username__icontains=query) \
                         .exclude(pk__in=followed_ids) \
                         .exclude(pk=request.user.pk)[:10]
@@ -78,4 +137,4 @@ def search_user(request):
         }
         for user in users
     ]
-    return JsonResponse(data, safe=False)
+    return JsonResponse(data)
