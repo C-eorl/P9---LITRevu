b0482f580c5264c7f0c6ad057170f19313f53536
ceorl <dev.rocher.florian@gmail.com>
2025-10-13 11:08:40 +0200

    modification parametre "ticket_id" => "pk" pour compatibilité ViewGeneric

--- litrevu/reviews/urls.py
+++ litrevu/reviews/urls.py
@@ -5,6 +5,7 @@ app_name = 'reviews'
 
 urlpatterns = [
     path("", FeedView.as_view(), name='feed'),
+    path("posts/", PostView.as_view(), name='posts'),
 
     # url pour views follow
     path("follow/", FollowView.as_view(), name='follow'),
@@ -13,10 +14,10 @@ urlpatterns = [
     path("search_user/", search_user, name="search_user"),
     # url pour views ticket
     path("tickets/create/", TicketCreateView.as_view(), name="ticket_create"),
-    path("tickets/<int:ticket_id>/modify/", TicketModifyView.as_view(), name="ticket_modify"),
-    path("tickets/<int:ticket_id>/delete/", TicketDeleteView.as_view(), name="ticket_delete"),
+    path("tickets/<int:pk>/modify/", TicketModifyView.as_view(), name="ticket_modify"),
+    path("tickets/<int:pk>/delete/", TicketDeleteView.as_view(), name="ticket_delete"),
     # url pour views critique
     path("review/create/", ReviewView.as_view(), name="review_create"),
-    path("review/<int:review_id>/modify/", ReviewModifyView.as_view(), name="review_create"),
-    path("review/<int:review_id>/delete/", ReviewDeleteView.as_view(), name="review_create"),
+    path("review/<int:pk>/modify/", ReviewModifyView.as_view(), name="review_modify"),
+    path("review/<int:pk>/delete/", ReviewDeleteView.as_view(), name="review_delete"),
 ]
