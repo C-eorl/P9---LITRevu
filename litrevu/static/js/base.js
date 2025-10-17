8e20ac51dfad5bd4e8cbd7bf7c171b8586a9c9b2
ceorl <dev.rocher.florian@gmail.com>
2025-10-17 15:17:55 +0200

    suppression base.js

--- litrevu/static/js/base.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const banner = document.getElementById('banner');
-    if (banner) {
-        banner.addEventListener('animationend', () => {
-            banner.style.display = 'none';
-        });
-    }
-});
