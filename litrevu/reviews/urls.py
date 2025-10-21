dde87d45d664ec59abcbfe3234be20902c25284f
ceorl <dev.rocher.florian@gmail.com>
2025-10-21 06:20:40 +0200

    modification des noms class Views

--- litrevu/reviews/urls.py
+++ litrevu/reviews/urls.py
@@ -16,11 +16,11 @@ urlpatterns = [
     path("search_user/", search_user, name="search_user"),
     # url pour views ticket
     path("tickets/create/", TicketCreateView.as_view(), name="ticket_create"),
-    path("tickets/<int:pk>/modify/", TicketModifyView.as_view(), name="ticket_modify"),
+    path("tickets/<int:pk>/modify/", TicketUpdateView.as_view(), name="ticket_modify"),
     path("tickets/<int:pk>/delete/", TicketDeleteView.as_view(), name="ticket_delete"),
     # url pour views critique
-    path("review/create/", ReviewView.as_view(), name="review_create"),
-    path("review/create/<int:ticket_id>/", ReviewView.as_view(), name="review_create_for_ticket"),
-    path("review/<int:pk>/modify/", ReviewModifyView.as_view(), name="review_modify"),
+    path("review/create/", ReviewCreateView.as_view(), name="review_create"),
+    path("review/create/<int:ticket_id>/", ReviewCreateView.as_view(), name="review_create_for_ticket"),
+    path("review/<int:pk>/modify/", ReviewUpdateView.as_view(), name="review_modify"),
     path("review/<int:pk>/delete/", ReviewDeleteView.as_view(), name="review_delete"),
 ]
