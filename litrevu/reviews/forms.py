0f95c61acd1a58b3ac146b9c5b38e3a140e3c9b6
ceorl <dev.rocher.florian@gmail.com>
2025-10-21 06:18:02 +0200

    implementation d'un formulaire 2in1 Review&Ticket Implementation formulaire Review

--- litrevu/reviews/forms.py
+++ litrevu/reviews/forms.py
@@ -1,15 +1,41 @@
 from django import forms
-
-from reviews.models import Review
+from .models import Review, Ticket
 
 
 class ReviewForm(forms.ModelForm):
-    """Formulaire pour les critiques"""
+    """Formulaire de base pour les critiques"""
 
-    # Ajout d'un champ pour créer un ticket en même temps (critique libre)
+    class Meta:
+        model = Review
+        fields = ['headline', 'rating', 'body']
+        widgets = {
+            'headline': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Titre de votre critique'
+            }),
+            'rating': forms.Select(
+                choices=[(i, '★' * i + '☆' * (5 - i)) for i in range(6)],
+                attrs={'class': 'form-control'}
+            ),
+            'body': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 4,
+                'placeholder': 'Votre critique'
+            })
+        }
+        labels = {
+            'headline': 'Titre',
+            'rating': 'Note',
+            'body': 'Commentaire'
+        }
+
+
+class ReviewWithTicketForm(ReviewForm):
+    """Formulaire pour créer une critique avec un nouveau ticket"""
+
     ticket_title = forms.CharField(
         max_length=128,
-        required=False,
+        required=True,
         widget=forms.TextInput(attrs={
             'class': 'form-control',
             'placeholder': 'Titre du livre/article'
@@ -33,21 +59,26 @@ class ReviewForm(forms.ModelForm):
         label='Image'
     )
 
-    class Meta:
-        model = Review
-        fields = ['headline', 'rating', 'body']
-        widgets = {
-            'headline': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Titre de votre critique'
-            }),
-            'rating': forms.Select(
-                choices=[(i, '★' * i + '☆' * (5 - i)) for i in range(6)],
-                attrs={'class': 'form-control'}
-            ),
-            'body': forms.Textarea(attrs={
-                'class': 'form-control',
-                'rows': 4,
-                'placeholder': 'Votre critique'
-            })
-        }
+    class Meta(ReviewForm.Meta):
+        pass
+
+    def save(self, commit=True, user=None):
+        """Crée le ticket et la review en une seule transaction"""
+
+        # Créer le ticket
+        ticket = Ticket.objects.create(
+            title=self.cleaned_data['ticket_title'],
+            description=self.cleaned_data.get('ticket_description', ''),
+            image=self.cleaned_data.get('ticket_image'),
+            user=user
+        )
+
+        # Créer la review
+        review = super().save(commit=False)
+        review.ticket = ticket
+        review.user = user
+
+        if commit:
+            review.save()
+
+        return review
