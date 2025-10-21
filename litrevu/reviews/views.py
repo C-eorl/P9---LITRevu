fa7bfb921e57e603079e531125a158fe45213660
ceorl <dev.rocher.florian@gmail.com>
2025-10-21 06:23:19 +0200

    docstrings et commentaire Rename class Refactorisation des class ReviewCreateView & ReviewUpdateView pour aller avec ReviewForm et ReviewWithTicketForm

--- litrevu/reviews/views.py
+++ litrevu/reviews/views.py
@@ -10,7 +10,7 @@ from django.urls import reverse_lazy
 from django.views.decorators.http import require_GET, require_POST
 from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView
 
-from .forms import ReviewForm
+from .forms import ReviewForm, ReviewWithTicketForm
 from .models import UserFollow, Ticket, Review, UserBlocked
 from authentication.models import User
 
@@ -36,12 +36,13 @@ class FeedView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
+        # Récupère les tickets / critiques des utilisateurs auxquels je suis abonnées.
         followers_users = UserFollow.objects.filter(user=self.request.user)\
                                             .values_list('following_user', flat=True)
         tickets = Ticket.objects.filter(user__in=followers_users)
-        tickets = tickets.annotate(content_type=Value("ticket",CharField()))
         reviews = Review.objects.filter(user__in=followers_users)
+        # Rajoute le champ content_type
+        tickets = tickets.annotate(content_type=Value("ticket", CharField()))
         reviews = reviews.annotate(content_type=Value("review",CharField()))
 
         post_followers_users = sorted(
@@ -49,8 +50,11 @@ class FeedView(TemplateView):
             key=attrgetter('time_created'),
             reverse=True
         )
-        blocked_me = UserBlocked.objects.filter(blocked_user=self.request.user) .values_list('user', flat=True)
+        # Récupère la liste des utilisateurs qui ont bloqué l'utilisateur connecté
+        blocked_me = UserBlocked.objects.filter(blocked_user=self.request.user).values_list('user', flat=True)
+        # Récupère la liste des ID de ticket déjà critiqué
         reviewed_ticket_ids = Review.objects.values_list('ticket_id', flat=True)
+
         context['blocked_me'] = blocked_me
         context['reviewed_ticket_ids'] = reviewed_ticket_ids
         context['list_posts'] = post_followers_users
@@ -64,9 +68,10 @@ class PostView(TemplateView):
         context = super().get_context_data(**kwargs)
 
         tickets = Ticket.objects.filter(user=self.request.user)
-        tickets = tickets.annotate(content_type=Value("ticket",CharField()))
         reviews = Review.objects.filter(user=self.request.user)
+        tickets = tickets.annotate(content_type=Value("ticket",CharField()))
         reviews = reviews.annotate(content_type=Value("review",CharField()))
+
         posts = sorted(
             chain(tickets, reviews),
             key=attrgetter('time_created'),
@@ -93,7 +98,7 @@ class TicketCreateView(CreateView):
         return response
 
 
-class TicketModifyView(UserTestCustom, UpdateView):
+class TicketUpdateView(UserTestCustom, UpdateView):
     """ View pour modifier un ticket """
     template_name = 'reviews/ticket_form.html'
     success_url = reverse_lazy('reviews:feed')
@@ -117,52 +122,64 @@ class TicketDeleteView(UserTestCustom, DeleteView):
 # ================================================================ #
 #                         Critique                                 #
 # ================================================================ #
-class ReviewView(CreateView):
-    """ View pour le la création d'une critique """
+class ReviewCreateView(CreateView):
+    """Vue pour créer une critique (en réponse à un ticket ou libre)"""
+
     template_name = 'reviews/review_form.html'
     success_url = reverse_lazy('reviews:feed')
     model = Review
-    form_class = ReviewForm
+
+    def get_form_class(self):
+        """Retourne le bon formulaire selon le contexte"""
+        if self.kwargs.get('ticket_id'):
+            # Réponse à un ticket existant
+            return ReviewForm
+        else:
+            # Critique libre (avec création de ticket)
+            return ReviewWithTicketForm
+
+    def get_tickets(self):
+        """ Récupère le ticket ou None"""
+        tickets_id = self.kwargs.get('ticket_id')
+        if tickets_id:
+            return get_object_or_404(Ticket, pk=tickets_id)
+        else:
+            return None
+
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        ticket_id = self.kwargs.get('ticket_id')
-        if ticket_id:
-            context['ticket'] = get_object_or_404(Ticket, id=ticket_id)
+
+        ticket = self.get_tickets()
+        if ticket:
+            context['ticket'] = ticket
+
         return context
 
     def form_valid(self, form):
-        form.instance.user = self.request.user
-        ticket_id = self.kwargs.get('ticket_id')
-        if ticket_id:
-            form.instance.ticket = get_object_or_404(Ticket, id=ticket_id)
+        """Traite le formulaire selon le type de critique"""
+        ticket = self.get_tickets()
+
+        if ticket:
+            # Réponse à un ticket existant
+            form.instance.ticket = ticket
+            form.instance.user = self.request.user
+            messages.success(self.request, 'Critique créée avec succès!')
+            return super().form_valid(form)
         else:
-            title = form.cleaned_data.get('ticket_title')
-            description = form.cleaned_data.get('ticket_description')
-            image = form.cleaned_data.get('ticket_image')
-
-            # Vérifie qu'un titre est fourni
-            if not title:
-                form.add_error('ticket_title', "Le titre est obligatoire pour créer une critique libre.")
-                return self.form_invalid(form)
-
-            new_ticket = Ticket.objects.create(
-                title=title,
-                description=description,
-                image=image,
-                user=self.request.user
-            )
-            form.instance.ticket = new_ticket
-        messages.success(self.request, 'Critique créée avec succès!')
-        return super().form_valid(form)
+            # Critique libre avec création de ticket
+            form.save(user=self.request.user)
+            messages.success(self.request, 'Critique créée avec succès!')
+            return super().form_valid(form)
 
 
-class ReviewModifyView(UserTestCustom, UpdateView):
-    """ View pour modifier une critique """
+class ReviewUpdateView(UserTestCustom, UpdateView):
+    """Vue pour modifier une critique existante"""
+
     template_name = 'reviews/review_form.html'
-    success_url = reverse_lazy('reviews:feed')
+    success_url = reverse_lazy('reviews:posts')
     model = Review
-    fields = ['headline', 'rating', 'body']
+    form_class = ReviewForm
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -170,8 +187,9 @@ class ReviewModifyView(UserTestCustom, UpdateView):
         return context
 
     def form_valid(self, form):
-        messages.success(self.request, 'Critique modifié avec succès!')
+        messages.success(self.request, 'Critique modifiée avec succès!')
         return super().form_valid(form)
+
 
 class ReviewDeleteView(UserTestCustom, DeleteView):
     """ View pour supprimer une critique """
@@ -192,7 +210,7 @@ class FollowView(TemplateView):
     template_name = 'reviews/follow.html'
 
     def get_context_data(self, **kwargs):
-        # récupere context_object_name
+        """ Récupère le context """
         context = super().get_context_data(**kwargs)
         context['followed_users'] = User.objects.filter(followers__user=self.request.user)
         context['followers'] = User.objects.filter(following__following_user=self.request.user)
@@ -211,7 +229,6 @@ def unfollow_user(request, user_id):
 @require_POST
 def follow_user(request):
     """ View pour s'abonner d'un utilisateur """
-    # TODO : refactoriser
     username = request.POST.get("username")
     if not username:
         return JsonResponse({"success": False, "error": "Nom d'utilisateur manquant."})
@@ -219,25 +236,6 @@ def follow_user(request):
     user_to_follow = User.objects.get(username=username)
     UserFollow.objects.get_or_create(user=request.user, following_user=user_to_follow)
     return JsonResponse({"success": True})
-
-@require_POST
-def blocked_user(request, user_id):
-    """ Bloque un utilisateur """
-    user_to_blocked = User.objects.get(pk=user_id)
-    UserBlocked.objects.get_or_create(user=request.user, blocked_user=user_to_blocked)
-    UserFollow.objects.filter(user=user_to_blocked, following_user=request.user).delete()
-
-    messages.success(request, f"l'utilisateur {user_to_blocked.username} a été bloqué(e)")
-    return redirect('reviews:follow')
-
-@require_POST
-def unblocked_user(request, user_id):
-    """ Débloquer un utilisateur """
-    unblocked_user = User.objects.get(pk=user_id)
-    UserBlocked.objects.filter(user=request.user, blocked_user=unblocked_user).delete()
-    messages.success(request, f"Vous avez débloqué {unblocked_user.username}")
-    return redirect('reviews:follow')
-
 
 @require_GET
 def search_user(request):
@@ -265,3 +263,23 @@ def search_user(request):
         for user in users
     ]
     return JsonResponse(data, safe=False)
+
+@require_POST
+def blocked_user(request, user_id):
+    """ Bloque un utilisateur """
+    user_to_blocked = User.objects.get(pk=user_id)
+    UserBlocked.objects.get_or_create(user=request.user, blocked_user=user_to_blocked)
+    UserFollow.objects.filter(user=user_to_blocked, following_user=request.user).delete()
+
+    messages.success(request, f"l'utilisateur {user_to_blocked.username} a été bloqué(e)")
+    return redirect('reviews:follow')
+
+@require_POST
+def unblocked_user(request, user_id):
+    """ Débloquer un utilisateur """
+    unblocked_user = User.objects.get(pk=user_id)
+    UserBlocked.objects.filter(user=request.user, blocked_user=unblocked_user).delete()
+    messages.success(request, f"Vous avez débloqué {unblocked_user.username}")
+    return redirect('reviews:follow')
+
+
